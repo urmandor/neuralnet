ca8274812537c6c0c42a166381964393264e4cfd
ursalan <ursalan.mansoor@objectloop.com>
2018-08-10 15:02:06 +0500

    Fix neural net

--- tutorial/AbstractNeuralNetwork.py
+++ tutorial/AbstractNeuralNetwork.py
@@ -1,13 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import h5py
+import copy
 # from testCases_v4 import *
 # from dnn_utils_v2 import sigmoid, sigmoid_backward, relu, relu_backward
 
 from abc import ABC, abstractmethod
 
 class AbstractNeuralNetwork(ABC):
+
     def __init__(self):
         super().__init__()
+        self.trainX, self.trainY = None, None 
+        self.testX, self.testY = None, None
+        self.classes = None
+        self.activations = None
+        self.layerdims = None
 
     def initializeParams(self, layerdims):
         """
@@ -21,7 +29,7 @@ class AbstractNeuralNetwork(ABC):
         parameters = {}
         L = len(layerdims)
         for l in range(1, L):
-            parameters["W" + str(l)] = np.random.randn(layerdims[l], layerdims[l-1])
+            parameters["W" + str(l)] = np.random.randn(layerdims[l], layerdims[l-1]) * 0.01
             parameters["b" + str(l)] = np.zeros((layerdims[l], 1))
 
         return parameters
@@ -96,11 +104,11 @@ class AbstractNeuralNetwork(ABC):
         cashes = []
         
         A = X
-        L = len(parameters)/2
+        L = len(parameters) // 2
         max_activation_index, activation_name = activations.pop(0)
 
         for l in range(1, L+1):
-
+            
             if l > max_activation_index and len(activations) > 0:
                 max_activation_index, activation_name = activations.pop(0)
 
@@ -126,13 +134,13 @@ class AbstractNeuralNetwork(ABC):
         """
 
         m = Y.shape[1]
-        cost = np.divide(np.dot(Y, np.log(yHat).T) + np.dot(1-Y, np.log(1-Y).T), -m)
+        cost = np.divide(np.dot(Y, np.log(yHat).T) + np.dot((1-Y), np.log(1-yHat).T), -m)
         cost = np.squeeze(cost)
         return cost
 
     # BACK PROPAGATION START ...
 
-    def backwardLinear(self, dA, cache, activation_type):
+    def backwardLinear(self, dA, Z, activation_type):
         """
             Implements the dZ part of backward propagation
 
@@ -144,9 +152,9 @@ class AbstractNeuralNetwork(ABC):
             returns:
             dZ -- Derivation of the linear part of the neural network
         """
-
+    
         activation_function = self.findDerivativeOfActivationImplementation(activation_type)
-        dZ = activation_function(dA, cache)
+        dZ = activation_function(dA, Z)
 
         return dZ
 
@@ -173,7 +181,7 @@ class AbstractNeuralNetwork(ABC):
             db -- derivative of b of current layer 
         """
         A_prev, A, Z, W, b = cache
-        m = A.shape[1]
+        m = A_prev.shape[1]
         dW = np.divide(np.dot(dZ, A_prev.T), m)
         db = np.divide(np.sum(dZ, keepdims=True, axis=1), m)
         dA_prev = np.dot(W.T, dZ)
@@ -195,22 +203,22 @@ class AbstractNeuralNetwork(ABC):
 
         gradients = {}
         L = len(caches)
-        m = Y.shape[1]
         Y = Y.reshape(yHat.shape)
 
         dAL = - (np.divide(Y, yHat) - np.divide(1-Y, 1-yHat))
-
         activations.reverse()
-
         max_activation_index, activation_name = activations.pop(0)
-
-        for l in reversed(range(1, L)):
+        dA = dAL
+        
+        for l in reversed(range(1, L+1)):
             
-            if l == max_activation_index and len(activations) > 0:
+            if l < max_activation_index and len(activations) > 0:
                 max_activation_index, activation_name = activations.pop(0)
 
-            cache = caches[l]
-            dZ = self.backwardLinear(dAL, cache, activation_name)
+            cache = caches[l-1]
+            A_prev, A, Z, W, b = cache
+            
+            dZ = self.backwardLinear(dA, Z, activation_name)
             dA, dW, db = self.backwardActivation(dZ, cache)
 
             gradients["dA" + str(l - 1)] = dA
@@ -234,27 +242,30 @@ class AbstractNeuralNetwork(ABC):
             parameters -- updated parameters
         """
 
-        L = len(parameters)/2
+        L = len(parameters) // 2
 
-        for l in range(1, L):
+        for l in range(1, L+1):
             parameters["W" + str(l)] = parameters["W" + str(l)] - np.multiply(learningRate, gradients["dW" + str(l)])
             parameters["b" + str(l)] = parameters["b" + str(l)] - np.multiply(learningRate, gradients["db" + str(l)])
 
         return parameters
     
     
-    def startTraining(self, X, Y, layerdims, activations, learningRate=0.0075, numIterations=3000, printCost=False, printInterval=100):
+    def startTraining(self, X, Y, layerdims=None, activations=None, learningRate=0.0075, numIterations=3000, printCost=False, printInterval=100):
         
         costs = []
+
+        layerdims = layerdims or self.layerdims
+        activations = activations or self.activations
+
         parameters = self.initializeParams(layerdims)
 
         for i in range(0, numIterations):
-            yHat, caches = self.forwardPropagation(X, parameters, activations)
+            yHat, caches = self.forwardPropagation(X, parameters, copy.deepcopy(activations))
 
             cost = self.calculateCost(yHat, Y)
 
-            gradients = self.backwardPropagation(yHat, Y, caches, activations)
-
+            gradients = self.backwardPropagation(yHat, Y, caches, copy.deepcopy(activations))
             parameters = self.updateParams(parameters, gradients, learningRate)
 
             if printCost and i % printInterval == 0:
@@ -269,3 +280,57 @@ class AbstractNeuralNetwork(ABC):
 
         return parameters
     
+    def loadData(self, trainingSrc, testSrc=None):
+
+        classes, test_set_x_orig, test_set_y_orig = None, None, None
+
+        train_dataset = h5py.File(trainingSrc, "r")
+        train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
+        train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
+        train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+
+        if testSrc:
+            test_dataset = h5py.File(testSrc, "r")
+            test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
+            test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
+
+            classes = np.array(test_dataset["list_classes"][:]) # the list of classes
+            test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
+        
+        self.trainX = train_set_x_orig
+        self.trainY = train_set_y_orig
+        self.testX = test_set_x_orig
+        self.testY = test_set_y_orig
+        self.classes = classes
+
+        return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
+
+    def setLayers(self, layerdims):
+        self.layerdims = layerdims
+
+    def exploreData(self):
+        # Explore your dataset 
+        m_train = self.trainX.shape[0]
+        num_px = self.trainX.shape[1]
+        m_test = self.testX and self.testX.shape[0]
+
+        print ("Number of training examples: " + str(m_train))
+        print ("Number of testing examples: " + str(m_test))
+        print ("Each image is of size: (" + str(num_px) + ", " + str(num_px) + ", 3)")
+        print ("train_x_orig shape: " + str(self.trainX.shape))
+        print ("train_y shape: " + str(self.trainY.shape))
+        print ("test_x_orig shape: " + str(self.testX and self.testX.shape))
+        print ("test_y shape: " + str(self.testX and self.testY.shape))
+
+    def flattenData(self):
+        self.trainX = self.trainX.reshape(self.trainX.shape[0], -1).T
+        self.testX = self.testX and self.trainX.reshape(self.trainX.shape[0], -1).T
+
+    def normalizeData(self, maxValue=255):
+        self.trainX = self.trainX/maxValue
+        self.testX = self.testX and self.testX/maxValue
+
+
+    def testNetwork(self):
+        np.random.seed(1)
+        return self.initializeParams([3,2,1])
--- tutorial/NNReluSigmoid.py
+++ tutorial/NNReluSigmoid.py
@@ -1,12 +1,12 @@
 import numpy as np
-from .AbstractNeuralNetwork import AbstractNeuralNetwork as ANN
+from AbstractNeuralNetwork import AbstractNeuralNetwork as ANN
 
 class NNReluSigmoid(ANN):
 
     def __init__(self):
         super().__init__()
 
-    def findActivationImplementation(self, activation_type):
+    def findActivationImplementation(self, activation_type):    
         if activation_type == "sigmoid":
             return self.sigmoid
         elif activation_type == "relu":
@@ -21,8 +21,6 @@ class NNReluSigmoid(ANN):
             return self.backwardRelu
         else:
             return self.backwardRelu
-        
-
 
     def sigmoid(self, Z):
         A = 1 / (1+np.exp(-Z))
@@ -32,14 +30,19 @@ class NNReluSigmoid(ANN):
         A = np.maximum(0, Z)
         return A
 
-    def backwardSigmoid(self, dA, cache):
-        prev_A, A, Z, W, b = cache
+    def backwardSigmoid(self, dA, Z):
         s = self.sigmoid(Z)
         dZ = dA * s * (1-s)
         return dZ
 
-    def backwardRelu(self, dA, cache):
-        prev_A, A, Z, W, b = cache
+    def backwardRelu(self, dA, Z):
         dZ = np.array(dA, copy=True)
-        dZ[Z<=0] = 0
+        dZ[Z<0] = 0
         return dZ
+
+    def setDefaultActivation(self, activations=None):
+        if activations:
+            self.activations = activations
+        else:
+            size = len(self.layerdims)
+            self.activations = [(size-2, 'relu'), (size-1, 'sigmoid')]
